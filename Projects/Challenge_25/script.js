33bbc5dddfde06134054be705dddadca10fb0c2d
Christian R <cjromerom28@gmail.com>
2025-09-16 18:17:51 -0500

    M1 Challenge 26 complete

--- Projects/Challenge_25/script.js
+++ Projects/Challenge_25/script.js
@@ -1,11 +1,25 @@
 const handleGreat = () => {
     const name = getInfo('name')
     const lastname = getInfo('lastname')
-    alert(name+ ' ' + lastname)
+    const age = getAge('age')
+    const stature = getStature('stature')
+    alert(name+ ' ' +lastname+' '+age+' '+stature)
 }
 
 const getInfo = (id) => {
     let data; 
     data = document.getElementById(id)
     return data.value
+}
+
+const getAge = (id) =>{
+    let age;
+    age = document.getElementById(id)
+    return parseInt(age.value)
+}
+
+const getStature = (id) => {
+    let stature;
+    stature = document.getElementById(id)
+    return parseFloat(stature.value)
 }
