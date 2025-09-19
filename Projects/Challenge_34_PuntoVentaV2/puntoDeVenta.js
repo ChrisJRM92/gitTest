5c262422112504171509cbce4f334072a49cfcee
Christian R <cjromerom28@gmail.com>
2025-09-19 16:13:30 -0500

    M1 Challenge 34 complete

---
+++ Projects/Challenge_34_PuntoVentaV2/puntoDeVenta.js
@@ -0,0 +1,62 @@
+calcularValorTotal = function () {
+    let nombreProducto;
+    let precioProducto;
+    let cantidad;
+    let porcentajeDescuento;
+
+
+    nombreProducto = document.getElementById('txtProducto').value
+    precioProducto = parseFloat(document.getElementById('txtPrecio').value)
+    cantidad = parseInt(document.getElementById('txtCantidad').value)
+
+    esProductoValido(nombreProducto);
+    esCantidadValida(cantidad);
+    
+
+    if (!precioProducto) {
+        document.getElementById('field_price').innerText = string_required
+    } else {
+        document.getElementById('field_price').innerText = ''
+    }
+
+    if (precioProducto > 0 && precioProducto < 50) {
+
+        valorSubtotal = calcularSubTotal(precioProducto, cantidad)
+        document.getElementById('lblSubtotal').innerText = valorSubtotal
+        valorDescuento = calcularDescuentoPorVolumen(valorSubtotal, cantidad)
+        document.getElementById('lblDescuento').innerText = valorDescuento
+        let opp = valorSubtotal - valorDescuento
+        valorIVA = parseFloat(calcularIVA(opp).toFixed(2))
+        console.log(valorIVA)
+        document.getElementById('lblValorIVA').innerText = valorIVA
+        console.log(valorSubtotal, valorDescuento, valorIVA)
+        valorTotal = parseFloat(calcularTotal(valorSubtotal, valorDescuento, valorIVA).toFixed(2));
+        document.getElementById('lblTotal').innerText = valorTotal;
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+limpiar = function (arr) {
+    for (let i = 0; i < arr.length; i++) {
+        document.getElementById(arr[i]).innerText = '0.0';
+    }
+    document.getElementById('txtProducto').value = '';
+    document.getElementById('txtCantidad').value = '';
+    document.getElementById('txtPrecio').value = '';
+
+    document.getElementById('field_product').innerText = '';
+    document.getElementById('field_amount').innerText = '';
+    document.getElementById('field_price').innerText = '';
+
+}
---
+++ Projects/Challenge_34_PuntoVentaV2/serviciosPuntoDeVenta.js
@@ -0,0 +1,67 @@
+
+let valorSubtotal;
+let valorDescuento;
+let valorIVA;
+let valorTotal;
+
+const string_required = 'Campo obligatorio'
+const max_value = 'Max. 10 caracteres'
+const cantidad_max = 'Max. 100 productos'
+
+const calcularIVA = (monto) => {
+    const valorIVA = (monto * 12) / 100
+    return valorIVA;
+};
+const calcularSubTotal = (precio, cantidad) => {
+    const totalPagar = precio * cantidad;
+    return totalPagar;
+};
+
+const calcularTotal = (subTotal, descuento, iva) => {
+    const totalPagar = subTotal - descuento + iva
+    return totalPagar;
+};
+
+const calcularDescuentoPorVolumen = (subTotal, cantidad) => {
+    let descuento;
+    let res;
+    if (cantidad < 3) {
+        descuento = 0;
+    } else if (cantidad >= 3 && cantidad <= 5) {
+        descuento = 3;
+    } else if (cantidad >= 6 && cantidad <= 11) {
+        descuento = 4;
+    } else {
+        descuento = 5;
+    }
+    return res = subTotal * descuento / 100
+
+}
+
+
+const esProductoValido = (nombreProducto) => {
+    if (!nombreProducto) {
+        document.getElementById('field_product').innerText = string_required
+    } else {
+        document.getElementById('field_product').innerText = ''
+    }
+    if (nombreProducto.length > 10) {
+        document.getElementById('field_product').innerText = max_value
+    }
+}
+
+const esCantidadValida = (cantidad) => {
+    if (!cantidad) {
+        document.getElementById('field_amount').innerText = string_required
+    } else {
+        document.getElementById('field_amount').innerText = ''
+    }
+
+    if (cantidad > 0 && cantidad <= 100) {
+        document.getElementById('field_amount').innerText = ''
+    } else {
+        document.getElementById('field_amount').innerText = cantidad_max
+    }
+
+
+}
