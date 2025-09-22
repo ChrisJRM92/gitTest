6e99fbd36402116388fc07f2642f8bd02c5871df
Christian R <cjromerom28@gmail.com>
2025-09-22 09:47:59 -0500

    Paso 1 completado

--- Projects/Challenge_40_Ahorcado/ahorcado.js
+++ Projects/Challenge_40_Ahorcado/ahorcado.js
@@ -1,9 +1,33 @@
-const esMayuscula = (caracter) =>{
-    if(caracter.charCodeAt(0) >= 65 && caracter.charCodeAt(0) <= 90){
+let palabraSecreta = ''
+
+const esMayuscula = (caracter) => {
+    if (caracter.charCodeAt(0) >= 65 && caracter.charCodeAt(0) <= 90) {
         return true;
-    }else{
+    } else {
         return false;
     };
 };
 
-console.log(esMayuscula('H'))
+const guardarPalabra = (inputWord) => {
+    //const inputWord = document.getElementById('txtSecreta').value
+    let validation = false;
+    let error = ''
+    if (inputWord.length == 5) {
+        for (let i = 0; i < inputWord.length; i++) {
+            //console.log(esMayuscula(inputWord[i]))
+            if(esMayuscula(inputWord[i])){
+                validation = true
+            }else{
+                error = 'Todos los caracteres deben ser en mayuscula'
+                break
+            }
+        }
+    }else{
+        error = 'La plabra no tiene 5 caracteres'
+    }
+
+    return error
+}
+
+console.log(guardarPalabra('PALAB'))
+console.log('La palabra es: '+palabraSecreta)
