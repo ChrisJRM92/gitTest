fa6c9f353c4cc137fc799c876f327c55f58e2951
Christian R <cjromerom28@gmail.com>
2025-09-25 13:17:21 -0500

    Advances

--- Projects/ChristianRomero_Evaluacion/orden.js
+++ Projects/ChristianRomero_Evaluacion/orden.js
@@ -1,7 +1,80 @@
 let usuarios = [];
+let id = 0;
+const errors = [{
+    error_length: '*Almenos 3 caracteres',
+    error_empy: '*El campo es obligatorio',
+    error_noNumber: '*Solo se aceptan numeros'
+}]
 
-const registrarUsuario= ()=>{
-    const user = recuperarTexto('name');
-    const age = recuperarTexto('age');
-    console.log(user, age )
+const validarNombre = (user) => {
+    let validation = false;
+    
+    if(user){
+        mostrarTexto('error_name', '')
+        if(user.length < 3){
+            mostrarTexto('error_name', errors[0].error_length)
+            return validation
+        }else{
+            validation = true;
+            return validation
+        }
+        
+    }else{
+        mostrarTexto('error_name', errors[0].error_empy)
+        return validation
+    }
+}
+
+const validarEdad = (age) =>{
+    let validation = false;
+    let edad = parseInt(age)
+
+    if(edad){
+        mostrarTexto('error_age', '')
+        if( typeof edad == 'string'){
+            mostrarTexto('error_age', errors[0].error_noNumber)
+            return validation
+        }else{
+            validation = true;
+            return validation
+        }
+        
+    }else{
+        mostrarTexto('error_age', errors[0].error_empy)
+        return validation
+    }
+}
+
+const registrarUsuario = () => {
+    const userInput = recuperarTexto('name').trim();
+    const ageInput = recuperarTexto('age').trim();
+
+    const user = userInput.charAt(0).toUpperCase()+userInput.slice(1).toLowerCase();
+    const age = ageInput.charAt(0).toUpperCase()+ageInput.slice(1).toLowerCase();
+
+    let isTrueUser = validarNombre(user)
+    let isTrueAge = validarEdad(age)
+    
+    if(isTrueUser && isTrueAge){
+        alert('Usuario agregado correctamente')
+        usuarios.push({
+            id: id+=1,
+            name: user,
+            age: age
+        })
+        console.log(usuarios)
+
+    }
+}
+
+const crearUsuarioTabla = () =>{
+    let tabla = '<table><thead><tr><th>Id</th><th>Nombre</th><th>Edad</th></tr></thead>'
+    for(let i=0; i<usuarios.length; i++){
+        tabla+='<tbody><tr>'+
+        '<td>'+usuarios[i].id+'</td>'+
+        '<td>'+usuarios[i].name+'</td>'+
+        '<td>'+usuarios[i].age+'</td>'
+    }
+    tabla+='</tr></tbody></table>'
+    document.getElementById('tablaUsuarios').innerHTML = tabla;
 }
